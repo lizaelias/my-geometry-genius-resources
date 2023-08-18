1db857d88a2ce9fbea82ce6e76218d72f8c8a447
lizaelias <elias221624@gmail.com>
2023-08-19 00:24:22 +0600

    compelete a eliplis card

--- area-calculator.js
+++ area-calculator.js
@@ -41,3 +41,43 @@ function calculatorRectangleArea() {
     rectangleSpan.innerText = rectangleArea;
    
 }
+
+function calculatorParallelogramArea() {
+    
+    const base = getInputValue('parallelogram-base');
+    console.log(base);
+    const height = getInputValue('parallelogram-height');
+    console.log(height);
+
+    const area = base * height;
+
+    setElementInnerText('parallelogram-area', area);
+
+}
+
+function calculatorEllipseArea() {
+    
+    const majorRadius = getInputValue('ellipse-first-radius');
+    console.log(majorRadius);
+
+    const secondRadius = getInputValue('ellipse-second-radius');
+    console.log(secondRadius);
+    const area = 3.14 * majorRadius * secondRadius;
+
+    setElementInnerText('ellipse-area', area);
+}
+
+function getInputValue(id) {
+    
+    const inputField = document.getElementById(id);
+    const inputFieldText = inputField.value;
+    const Value = parseFloat(inputFieldText);
+    return Value;
+}
+
+function setElementInnerText(fieldId,value) {
+    const element = document.getElementById(fieldId);
+    element.innerText = value;
+
+}
+
